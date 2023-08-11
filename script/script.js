4df4c889fac387ed330b0fcaf2b31a590aafd54e
Oxygeeeen <abdulbaasitisah436@gmail.com>
2023-08-11 16:19:04 +0100

    Updates on delete key

--- script/script.js
+++ script/script.js
@@ -201,7 +201,7 @@ document.addEventListener("DOMContentLoaded", () => {
         clearDisplay();
         updateDisplay();
       } else if (buttonType === "delete") {
-        deLete()
+        deLete();
         updateDisplay();
       }
     });
